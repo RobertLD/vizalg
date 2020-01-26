ddd07ac3a267fdcfcf0b7f6e69fa198c19c4baa4
Robert DeRienzo <robert.derienzo@knights.ucf.edu>
2020-01-25 21:39:05 -0500

    Working bar graph w/ dumby data

--- page.py
+++ page.py
@@ -1,11 +1,37 @@
 from flask import Flask, redirect, url_for, render_template
+import plotly
+import plotly.graph_objs as go
+
+import pandas as pd
+import numpy as np
+import json
 
 app = Flask(__name__)
+def create_plot():
+
+
+    N = 40
+    x = np.linspace(0, 1, N)
+    y = np.random.randn(N)
+    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+
+
+    data = [
+        go.Bar(
+            x=df['x'], # assign x as the dataframe column 'x'
+            y=df['y']
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
 
 
 @app.route("/<name>")
 def home(name):
-    return render_template("index.html", content=name)
+    bar = create_plot()
+    return render_template("index.html", content=bar)
 
 
 if __name__ == "__main__":

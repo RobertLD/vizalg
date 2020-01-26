df9814acc3a05b0765a1458023803430a8a87d08
Robert DeRienzo <robert.derienzo@knights.ucf.edu>
2020-01-26 14:07:09 -0500

    Display randomly generated array of values to be sorted (in a future commit)

--- page.py
+++ page.py
@@ -1,37 +1,52 @@
 from flask import Flask, redirect, url_for, render_template
 import plotly
 import plotly.graph_objs as go
+import plotly.express as px
+from random import randint
 
 import pandas as pd
 import numpy as np
 import json
 
 app = Flask(__name__)
-def create_plot():
 
+# generate the animated plot for the array values
+def create_plot(unsorted):
 
-    N = 40
-    x = np.linspace(0, 1, N)
-    y = np.random.randn(N)
-    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+    # Animation function uses name matching
+    # TODO: Use custom algorithm to fill graphable dataframe with values, and them read them using 
+    # plotly express.
 
+    figure = px.bar(unsorted, x=range(0,len(unsorted)), y = unsorted, range_y=[0,1100], width = 1000, height = 500)
 
-    data = [
-        go.Bar(
-            x=df['x'], # assign x as the dataframe column 'x'
-            y=df['y']
-        )
-    ]
-
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+  
+    graphJSON = json.dumps(figure, cls=plotly.utils.PlotlyJSONEncoder)
 
     return graphJSON
 
+def selectionSort(unsorted):
 
-@app.route("/<name>")
-def home(name):
-    bar = create_plot()
-    return render_template("index.html", content=bar)
+
+    return 0
+
+
+# Generates the landing page
+# TODO: Convert index.html to be more of a landing page then a placeholder for
+# graphs
+
+#@app.route("/")
+#def home():
+#    bar = create_plot()
+#    return render_template("index.html", content=bar)
+
+@app.route("/selectionsort")
+def selsort():
+    # generate array of random values to
+    array = np.random.randint(1,1000, 1000)
+    #for i in range(0,1000):
+    #    array[i] = randint(0,1000)
+    bar = create_plot(array)
+    return render_template("index.html", unsorted = bar)
 
 
 if __name__ == "__main__":
